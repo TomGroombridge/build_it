2ca37acc98100f98b2fae0d46db13c05e43e9103
TomGroombridge <groombridgetom@gmail.com>
2015-06-12 13:46:07 +0100

    tests passing for first test of creating a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,17 @@ class ProductsController < ApplicationController
 		@product = Product.new
 	end
 
+	def create
+		@product = Product.create(params[:product].permit(:name, :category, :price, :stock))
+    if @product.save
+      redirect_to '/products'
+    else
+      render new_post_path
+    end
+	end
+
+	def index
+		@products = Product.all
+	end
+
 end
--- spec/features/adding_product_spec.rb
+++ spec/features/adding_product_spec.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
 
 describe 'adding a product' do
-    it 'should be able to add a new product' do
-      visit 'products/new'
-      fill_in 'name', with: 'Suede Shoes, Blue'
-      fill_in 'category', with: 'Women’s Footwear'
-      fill_in 'price', with: '42.00'
-      fill_in 'stock', with: '4'
-      attach_file 'Image', Rails.root.join('spec/images/plane.jpg')
-      click_button 'Create'
-      expect(current_path).to eq '/products'
-      expect(current_path).to have_content 'Suede Shoes, Blue'
-      # expect(page).to have_css 'img.christagram-upload'
-    end
+  it 'should be able to add a new product' do
+    visit 'products/new'
+    fill_in 'Name', with: 'Suede Shoes, Blue'
+    fill_in 'Category', with: 'Women’s Footwear'
+    fill_in 'Price', with: '42.00'
+    fill_in 'Stock', with: '4'
+    click_button 'Create Product'
+    expect(current_path).to eq '/products'
+    expect(page).to have_content 'Suede Shoes, Blue'
+  end
 
 end
