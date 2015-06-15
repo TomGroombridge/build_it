3b28468bcf34418564d853fd1f5dd82656fb7dff
TomGroombridge <groombridgetom@gmail.com>
2015-06-15 12:28:09 +0100

    added a cart which showed all the items that had been added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :products
+  resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
