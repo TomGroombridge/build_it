b49b284bcf02fffb746e9c3bac625ffb3ec558d9
TomGroombridge <groombridgetom@gmail.com>
2015-06-17 18:11:41 +0100

    test structure is using development db until can work out issue with test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	has_many :order_items
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'shoe.jpeg'
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'shoe.png'
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
--- spec/features/adding_to_cart_spec.rb
+++ spec/features/adding_to_cart_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 
 describe 'adding a product' do
 
-	before do
-		create(:product)
-	end
 
   it 'should be able to add a new product' do
     visit '/products'
     expect(page).to have_content '$10.50'
   end
 
-
-
-  it 'should be able to add the product to cart', :js => true do
+  it 'should be able to add the product to cart', :js => true  do
   	visit '/products'
-    click_link('Add to Cart')
-
+    click_button("5-product")
+    visit '/cart'
+    expect(page).to have_content 'new shoes pink'
 	end
 
 end
