966bd34671b7260d4c1b83528db2911e48fdad49
TomGroombridge <groombridgetom@gmail.com>
2015-06-17 13:24:51 +0100

    voucher codes updating correctly

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -14,6 +14,13 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+    vouchers = Voucher.all
+    @vouchers = []
+    vouchers.map  do |v|
+      if v.price_of_activation <= @order.subtotal
+        @vouchers << v
+      end
+    end
   end
 
   def destroy
