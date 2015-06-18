5ea276aceb0b98a16476f5735b6846edbbce7c14
TomGroombridge <groombridgetom@gmail.com>
2015-06-18 02:30:22 +0100

    seeded database

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.0'
 
 gem 'database_cleaner'
 
+gem 'seed_dump'
+
 gem 'selenium-webdriver'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!([
+  {name: "Almond Toe Court Shoes, Patent Black", price: 99.0, stock: 5, category: "womens_footwear", avatar_file_name: "womenshoe.jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 16646, avatar_updated_at: "2015-06-18 01:10:29"},
+  {name: "Suede Shoes, Blue", price: 42.0, stock: 4, category: "womens_footwear", avatar_file_name: "blue-suede-shoes-women-m4esnvyc.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 13593, avatar_updated_at: "2015-06-18 01:11:34"},
+  {name: "Leather Driver Saddle Loafers, Tan", price: 34.0, stock: 12, category: "mens_footwear", avatar_file_name: "cole-haan-saddle-tan-cole-haan-mens-howland-penny-loafer-product-1-2961596-409458305.jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 151431, avatar_updated_at: "2015-06-18 01:12:37"},
+  {name: "Flip Flops, Red", price: 19.0, stock: 6, category: "mens_footwear", avatar_file_name: "havaianas-flip-flops-havaianas-top-flip-flops-ruby-red.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 86087, avatar_updated_at: "2015-06-18 01:13:43"},
+  {name: "Flip Flops, Blue", price: 19.0, stock: 0, category: "mens_footwear", avatar_file_name: "havaianas-flip-flops-havaianas-top-flip-flops-blue-sky.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 81600, avatar_updated_at: "2015-06-18 01:17:15"},
+  {name: "Gold Button Cardigan, Black", price: 167.0, stock: 6, category: "womens_casualwear", avatar_file_name: "380ee758-bf39-4aea-becb-e715772c88b4.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 56053, avatar_updated_at: "2015-06-18 01:18:25"},
+  {name: "Cotton Shorts, Medium Red", price: 30.0, stock: 5, category: "womens_casualwear", avatar_file_name: "img-thing.jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 20553, avatar_updated_at: "2015-06-18 01:20:47"},
+  {name: "Fine Stripe Short Sleeve Shirt, Grey", price: 49.0, stock: 9, category: "mens_casualwear", avatar_file_name: "01J521290011_017_Z1.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 243950, avatar_updated_at: "2015-06-18 01:24:00"},
+  {name: "Fine Stripe Short Sleeve Shirt, Green", price: 39.99, stock: 3, category: "mens_casualwear", avatar_file_name: "940468a2-ae87-4bee-8234-946f4f62989c.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 3320161, avatar_updated_at: "2015-06-18 01:25:00"},
+  {name: "Sharkskin Waistcoat, Charcoal", price: 75.0, stock: 2, category: "mens_formalwear", avatar_file_name: "000367412.jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 22161, avatar_updated_at: "2015-06-18 01:26:12"},
+  {name: "Lightweight Patch Pocket Blazer, Deer", price: 175.0, stock: 1, category: "mens_formalwear", avatar_file_name: "reiss-deer-lightweight-patch-pkt-blazer-product-1-4269143-518146599_large_flex.jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 82253, avatar_updated_at: "2015-06-18 01:27:12"},
+  {name: "Bird Print Dress, Black", price: 270.0, stock: 10, category: "womens_formalwear", avatar_file_name: "img-thing_(1).jpeg", avatar_content_type: "image/jpeg", avatar_file_size: 18492, avatar_updated_at: "2015-06-18 01:27:54"},
+  {name: "Mid Twist Cut-Out Dress, Pink", price: 540.0, stock: 5, category: "womens_formalwear", avatar_file_name: "125654260.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 11230, avatar_updated_at: "2015-06-18 01:29:08"}
+])
