161db0c9f2bab5ca905da18efb5097a6f0c45af0
TomGroombridge <groombridgetom@gmail.com>
2015-06-18 01:30:38 +0100

    added another cart test for removing one item and updating total

--- spec/features/remove_product_from_cart_spec.rb
+++ spec/features/remove_product_from_cart_spec.rb
@@ -4,8 +4,6 @@ describe 'removing a product' do
 
   before(:all) do
     create(:product)
-    # create(:order_item)
-    # create(:order)
   end
 
   it 'should be able to remove a item from the cart', :js => true do
@@ -13,10 +11,22 @@ describe 'removing a product' do
     click_button("product-1")
     click_link("helllo")
     expect(page).to have_content 'Red Shoes'
-    within ".remove" do
+    within ".order-items" do
       click_link("Remove", :match => :first)
     end
     expect(page).to have_content 'Nothing in Cart'
+  end
+
+  it "should remove one item from the cart and reduce the total by that much", :js => true do
+    visit '/products'
+    click_button("product-1")
+    click_button("product-1")
+    click_link("helllo")
+    expect(page).to have_content '$21.00'
+    within ".order-items" do
+      click_link("Remove", :match => :first)
+    end
+    expect(page).to have_content '$10.50'
   end
 
 end
